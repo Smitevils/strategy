de7bf4005ea4092390292c30577d48e07cbfe46d
smitevils <smitevils@yandex.ru>
2015-09-09 15:09:03 +0300

    верстаю первый блок

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Document</title>
+	<!--  Отключить выделение номера телефона в браузере на iPhone -->
+	<meta name="format-detection" content="telephone=no">
+	<!-- Styles -->
+	<link rel="stylesheet" href="assets/styles/main.css">
+	<link rel="stylesheet" href="assets/modules/fullPage/jquery.fullPage.css">
+	<!-- favicon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/assets/img/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="assets/img/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	<!-- /favicon -->
+</head>
+<body>
+
+	<div id="fullpage">
+		<div class="section section-bg-1">
+			<div class="table-block">
+				<div class="table-cell-block">dsgsdg</div>
+			</div>
+		</div>
+		<div class="section section-bg-2">2</div>
+		<div class="section section-bg-3-1">3</div>
+		<div class="section section-bg-3-2">4</div>
+		<div class="section section-bg-4">5</div>
+	</div>
+	<!-- Scripts -->
+	<script src="assets/scripts/js/jquery-2.1.4.min.js"></script>
+	<script src="assets/modules/fullPage/jquery.fullPage.min.js"></script>
+	<script src="assets/scripts/js/main.js"></script>
+	<script src="assets/scripts/js/jquery.mask.min.js"></script>
+	<!-- /Scripts -->
+</body>
+</html>
